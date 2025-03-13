9a13395fa2dea39f2fdd82ee5b4d7d6236a46096
Sachintha-Thilakarathna <thilakarathnasachintha@gmail.com>
2025-03-13 14:32:19 +0530

    commit 2

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,45 @@
-Fname = "sachintha";
-Lname = "thilakarathna";
-age = 22;
-number = 759917495;
+// Fname = "sachintha";
+// Lname = "thilakarathna";
+// age = 22;
+// number = 759917495;
 
-console.log("my name is" + " " + Fname)
-console.log("my last name is" + " " + Lname)
-console.log("my age is" + " " + age)
-console.log("my phone number is" + " " + number)
+// console.log("my name is" + " " + Fname)
+// console.log("my last name is" + " " + Lname)
+// console.log("my age is" + " " + age)
+// console.log("my phone number is" + " " + number)
 
-const num1 = Number(prompt("Enter number 01"));
-const num2 = Number(prompt("Enter number 02"));
-const operater = prompt("enter operater \n +,-,*,% ");
+// const num1 = Number(prompt("Enter number 01"));
+// const num2 = Number(prompt("Enter number 02"));
+// const operater = prompt("enter operater \n +,-,*,**,/,% ");
 
 
-if(operater == "+"){
-    console.log(num1+num2);
-}
-else if(operater == "-"){ 
-    console.log(num1-num2);
-}
-else if(operater == "*"){
-    console.log(num1*num2);
-}
-else if(operater == "%")
-{
-    console.log(num1%num2);
-}
+// if(operater === "+"){
+//     alert(num1+num2); 
+// }
+// else if(operater === "-"){ 
+//     alert(num1-num2);
+// }
+// else if(operater === "*"){
+//     alert(num1*num2);
+// }
+// else if(operater === "**"){
+//     alert(num1**num2);
+// }
+// else if(operater === "/")
+//     {
+//         alert(num1%num2);
+//     }
+// else if(operater === "/")
+// {
+//     alert(num1/num2);
+// }
+
+
+
+function calage (birthyear) {
+    const age = (2025-birthyear);
+    return(age);
+}  
+
+const age = calage(2002);
+console.log(age);
