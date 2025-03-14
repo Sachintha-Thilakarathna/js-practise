7a828e8315dc406b790188142f66257a32691681
Sachintha-Thilakarathna <thilakarathnasachintha@gmail.com>
2025-03-14 15:47:33 +0530

    til activity 6

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,8 +11,6 @@
 // const num1 = Number(prompt("Enter number 01"));
 // const num2 = Number(prompt("Enter number 02"));
 // const operater = prompt("enter operater \n +,-,*,**,/,% ");
-
-
 // if(operater === "+"){
 //     alert(num1+num2); 
 // }
@@ -33,13 +31,89 @@
 // {
 //     alert(num1/num2);
 // }
+// function calage (birthyear) {
+//     const age = (2025-birthyear);
+//     return(age);
+// }  
+// const age = calage(2002);
+// console.log(age);
+
+    // age = Number(prompt("enter your age"))
+    // if(age >=18){
+    //     alert("you are allowed to enter!");
+    // }
+    // else(age >17);{
+    //     alert("you must be 18 or older!");
+    // }
 
 
+// function canceled(ok){
+//  ok = confirm("Are you sure you want to delete this item?")
+//  if(ok == true){
+//     alert("Item deleted successfully.")}
+  
+//  else(ok == false);{
+//     alert("Action canceled.")}
+    
+//  }
 
-function calage (birthyear) {
-    const age = (2025-birthyear);
-    return(age);
-}  
+// const ok = canceled(true)
+// canceled(ok)
 
-const age = calage(2002);
-console.log(age);
+// function oddEven (num01){
+//    num01 = Number(prompt("Enter a number"))
+//    if(num01 %0){
+//       alert("Number is even");
+//    }
+//    else(num01 %1);{
+//       alert("Number is odd");
+//    }
+// }
+// oddEven();
+
+// function favColor (color){
+//    color = prompt("What is your favourite color?")
+//    if(color === blue){
+//       alert("Wow! Blue is cool!")
+//    }
+//    else if(color === red){
+//       alert("Red is full of energy!")
+//    }
+//    else{
+//       alert("That's a nice color too!")
+//    }
+// } 
+
+//  favColor();
+
+// function calculator(num01,num02){
+//    num01 = Number(prompt("Enter number 01"))
+//    num02 = Number(prompt("Enter number 02"))
+//    operator = prompt("Enter operator \n +, -, *, /")
+//    if(operator == "+"){
+//       alert(num01+num02)
+//    }
+//    else if(operator == "-"){
+//       alert(num01-num02)
+//    }
+//    else if(operator == "*"){
+//       alert(num01*num02)
+//    }
+//    else(operator == "/");{
+//       alert(num01/num02)
+//    }
+// }
+
+// calculator()
+
+// function secretNumber(number) {
+//    number = Number(prompt("Enter your lucky number"))
+//    if(number === 7){
+//       alert("Congratulations! You guessed it!")
+//    }
+//    else{
+//       alert("Wrong guess! Try again.")
+//    }
+// }
+
+// secretNumber()
